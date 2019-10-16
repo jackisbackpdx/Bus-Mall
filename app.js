2e812cf9818866438cef621ba77b1bee4f58670b
Jackson Kelley <jackson.kelley.g@gmail.com>
2019-10-16 10:34:37 -0700

    increases times shown and the times clicked and adds them into an array

--- app.js
+++ app.js
@@ -4,27 +4,32 @@ import { MallArray } from './ItemArray.js';
 const inputTags = document.querySelectorAll('input');
 const imageTags = document.querySelectorAll('img');
 const itemName = document.getElementsByClassName('name');
+
+let timesShownArr = [];
+let timesClickedArr = [];
+
 const item = new MallArray(busMall);
 
-let gamecount = 0;
+let choiceCount = 0;
 
 let randomProduct;
 let randomProduct2;
 let randomProduct3;
 
 const newProductButton = () => {
-
+    
     const randomProductFunction = () => {
         randomProduct = item.getRandomItem();
         randomProduct2 = item.getRandomItem();
         randomProduct3 = item.getRandomItem();
-    
+        
         while (randomProduct.id === randomProduct2.id) {
             randomProduct2 = item.getRandomItem();
         }  
         while (randomProduct3.id === randomProduct.id || randomProduct3.id === randomProduct2.id) {
             randomProduct3 = item.getRandomItem();
         }
+
         imageTags.forEach((image, i) => {
             if (i === 0) {
                 image.src = randomProduct.image;
@@ -35,26 +40,57 @@ const newProductButton = () => {
             }
         });
     };
-    inputTags.forEach((input, i) => {
+
+    const addInputValues = () => inputTags.forEach((input, i) => {
         if (i === 0) {
             input.value = randomProduct.id;
-        } else if (i === 0) {
-            input.value = randomProduct.id;
-        } else if (i === 0) {
-            input.value = randomProduct.id;
+            console.log(input.value);
+        } else if (i === 1) {
+            input.value = randomProduct2.id;
+            console.log(input.value);
+        } else if (i === 2) {
+            input.value = randomProduct3.id;
+            console.log(input.value);
         }
     });
-    
 
+    
+    
     randomProductFunction();
+    addInputValues();
+
+
+    const increaseTimesShown = () => {
+        randomProduct.timesShown++;
+        randomProduct2.timesShown++; 
+        randomProduct3.timesShown++;
+    };
+    
     
     inputTags.forEach((input) => {
         input.addEventListener('click', () => {
-            console.log('chosen');
             randomProductFunction();
-            gamecount++;
-            
+            addInputValues();
+            increaseTimesShown();
+            if (choiceCount === 25) {
+                busMall.forEach((item) => {
+                    timesClickedArr.push(item.timesShown);
+                    timesShownArr.push(item.timesClicked);
+                    console.log('times clicked');
+                    console.log('times shown');
+                    console.log(timesClickedArr);
+                    console.log(timesShownArr);
+                });
+            }
+            choiceCount++;
+            console.log(choiceCount);
+            busMall.forEach((item) => {
+                if (item.id === input.value) {
+                    item.timesClicked++;
+                }
+            });
         });
     });
 };
+
 newProductButton();
--- test/products.test.js
+++ test/products.test.js
@@ -1,5 +1,5 @@
 import { MallArray } from '../ItemArray.js';
-import { busMall }
+import { busMall } from '../api.js';
 // IMPORT MODULES under test here:
 // import example from '../src/example.js';
 
@@ -8,7 +8,7 @@ const test = QUnit.test;
 test('time to test a function', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-
+    
     //Act 
     // Call the function you're testing and set the result to a const
 
